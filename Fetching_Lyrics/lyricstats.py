c6ec4322d58796fb129f6581944dce0c7a576608
prvn386 <53144668+prvn386@users.noreply.github.com>
2021-01-08 22:58:57 +0530

    Update lyricstats.py

--- Fetching_Lyrics/lyricstats.py
+++ Fetching_Lyrics/lyricstats.py
@@ -9,3 +9,13 @@ url = 'https://api.lyrics.ovh/v1/'+ artist + '/' + title
 
 #fetch lyrics
 response = request.get(url)
+json_data = json.loads(response.content)
+lyrics = json_data['lyrics']
+
+# determine how many keywords there in lyircs
+statstics = {}
+
+for keyword in keywords:
+    statstics[keyword] = lyrics.count(keyword)
+
+print(statstics)
